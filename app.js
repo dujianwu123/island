3338626d8680292789ceebbd14a78dca6688e60f
dujianwu123 <dujianwu123@126.com>
2019-11-20 17:29:24 +0800

    优化下app.js里的代码，抽离到core下

--- app.js
+++ app.js
@@ -1,22 +1,7 @@
 const koa = require('koa')
-const Router = require('koa-router')
-/**路由自动注入 */
-const requireDirectory = require('require-directory')
-// const book = require('./api/v1/book')
-// const classic = require('./api/v1/classic')
+const InitManager = require('./core/init')
 
 const app = new koa()
-
-// api文件夹下的文件自动注入，用于路由的自动注入
-const modules = requireDirectory(module, './app/api', {
-  visit: whenLoadModule
-})
-function whenLoadModule (obj) {
-  if (obj instanceof Router) {
-    app.use(obj.routes())
-  }
-}
-// app.use(book.routes())
-// app.use(classic.routes())
-
+process.cwd()
+InitManager.initCore(app)
 app.listen(3000)
